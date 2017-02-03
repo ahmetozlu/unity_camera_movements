859aef9c2a335bcad89bc556726fd267dca94833
ahmet.ozlu <ahmetozlu93@gmail.com>
2017-02-03 10:18:38 +0200

    some changes happening here.

--- MoveCamera.cs
+++ MoveCamera.cs
@@ -1,150 +1,272 @@
-﻿
-
-#define ISTABLET // Tablet,telefon ve masaüstü uygulamalarında VR uygulaması farklı tasarlandı.
-                // Telefon : Google cardboard ile kullanılacak
-                // Masaüstü : Haritadan konum seçilecek. Yön tuşları kullanılarak VR sahne içinde gezinme yapmayı sağlayacak.
-                // Tablet : Ekrana konulan yön tuşları ile VR sahne içinde gezinme sağlayacak.
-
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 
-public class MoveCamera : MonoBehaviour {
+enum MouseButtonDown {
+	MBD_LEFT = 0,
+	MBD_RIGHT,
+	MBD_MIDDLE,
+};
 
+[ RequireComponent( typeof( Camera ) ) ]
+public class MainCamera : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject focusObj = null; 
+	
+	private Vector3 oldPos; 
+	public int zoomfactor = 10;
+	private Vector3		initFocusPosition; // focusObj
+	private Quaternion	initFocusRotation;
+	private Vector3     initCamPosition;
+	private Quaternion  initCamRotation;
+
+	public Text text; // text is for main camera object position on scene
 	public float speed = 10.0f;
 	public float rotationSpeed = 100.0f;
-
-    int horizontal = 0, vertical = 0;
-    int flyFactor = 0;
-    float translation = 0.0f;
-
-   // public bool isTablet = true;
 	
-	// Use this for initialization
-	void Start () {
-	
+	int horizontal = 0, vertical = 0;
+	int flyFactor = 0;
+	float translation = 0.0f;
+
+	void setupFocusObject( string name ) {
+		GameObject obj = this.focusObj = new GameObject(name);
+		obj.transform.position = Vector3.zero;
+		
+		return;
 	}
 	
-	// Update is called once per frame
-	void Update ()
-    {
-        
-        if (Input.GetKey("escape"))
-            Application.Quit();
+	void Start() {
+		if( this.focusObj == null )
+			this.setupFocusObject( "CameraFocusObject" );
+		
+		Transform trans = this.transform;
+		transform.parent = this.focusObj.transform;
+		
+		trans.LookAt( this.focusObj.transform.position );
+		
+		// store init param
+		storeInitCamera();
+		
+		return;
+	}
+	
+	void Update() {
+		Vector3 playerPos =  GameObject.Find("Main Camera").transform.position;
+		text.text=playerPos.ToString("G4");
+		GetComponent<Camera>().fieldOfView += Input.GetAxis ("Mouse ScrollWheel") * zoomfactor;//ZOOMIN/OUT FARE ıLE
 
-//#if ISTABLET
+		if (Input.GetKey("escape"))
+			Application.Quit();
+		
+		//#if ISTABLET
+		
+		float translation = vertical * speed;
+		float rotation = horizontal * rotationSpeed;
 
-        float translation = vertical * speed;
-        float rotation = horizontal * rotationSpeed;
-        // reset
-        // Bu işlem yapılmazsa bir kez sağ tuşuna basılınca horizontal değeri 0 dan farklı 
-        // olacağından sonsuza kadar rotation işlemi yapılır.
-        horizontal = 0;
-        vertical = 0;
+		horizontal = 0;
+		vertical = 0;
+		
+		if (Mathf.Abs(translation - 0.0f) < 0.00001f)
+		{            //#else
+			translation = Input.GetAxis("Vertical") * speed;
+		}
+		
+		if (Mathf.Abs(rotation - 0.0f) < 0.00001f)
+		{
+			rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+		}
+		
+		translation *= Time.deltaTime;
+		rotation *= Time.deltaTime;
+		
+		float translationY = flyFactor * speed * Time.deltaTime;
+		transform.Translate(0, translationY, translation);
+		flyFactor = 0;
+		transform.Rotate(0, rotation, 0);
+		
+		this.mouseEvent();
+		
+		if( Input.GetKeyDown( KeyCode.Space ) ) {
+			this.restoreInitCamera();
+		}
+		
+		return;
+	}
+	
+	void storeInitCamera() {
+		initFocusPosition = this.focusObj.transform.position;
+		initFocusRotation = this.focusObj.transform.rotation;
+		initCamPosition = transform.position;
+		initCamRotation = transform.rotation;
+	}
+	
+	void restoreInitCamera() {
+		this.focusObj.transform.position = initFocusPosition;
+		this.focusObj.transform.rotation = initFocusRotation;
+		transform.position = initCamPosition;
+		transform.rotation = initCamRotation;
+	}
+	
+	void mouseEvent() {
+		float delta = Input.GetAxis("Mouse ScrollWheel");
+		if( delta != 0.0f )
+			this.mouseWheelEvent( delta );
+		
+		if( Input.GetMouseButtonDown( ( int )MouseButtonDown.MBD_LEFT ) ||
+		   Input.GetMouseButtonDown( ( int )MouseButtonDown.MBD_MIDDLE ) ||
+		   Input.GetMouseButtonDown( ( int )MouseButtonDown.MBD_RIGHT ) )
+			this.oldPos = Input.mousePosition;
+		
+		this.mouseDragEvent( Input.mousePosition );
+		
+		return;
+	}
+	
+	void mouseWheelEvent( float delta ) {
+		
+		Vector3 focusToPosition = this.transform.position - this.focusObj.transform.position;
+		
+		Vector3 post = focusToPosition * (1.0f + delta * -1.0f );
+		
+		if( post.magnitude > 0.01f )
+			this.transform.position = this.focusObj.transform.position + post;
+		
+		return;
+	}
+	
 
-        if (Mathf.Abs(translation - 0.0f) < 0.00001f)
-        {            //#else
-            translation = Input.GetAxis("Vertical") * speed;
-        }
+	void mouseDragEvent( Vector3 mousePos ) {
+		Vector3 diff = mousePos - oldPos;
+		
+		if( diff.magnitude < Vector3.kEpsilon )
+			return;
+		
+		if( Input.GetMouseButton( ( int )MouseButtonDown.MBD_LEFT ) ) {
+			this.cameraRotate( new Vector3( diff.y, diff.x, 0.0f ) );
+		} else if( Input.GetMouseButton( ( int )MouseButtonDown.MBD_MIDDLE ) ) {
+			this.cameraTranslate( -diff / 65.0f );
+		} else if( Input.GetMouseButton( ( int )MouseButtonDown.MBD_RIGHT ) ) {
+			Vector3 focusToPosition = this.transform.position - this.focusObj.transform.position;
+			Vector3 post = focusToPosition * ( 1.0f + diff.magnitude / 100 * ( ( diff.x >= 0 ) ? -1.0f : 1.0f ) );
+			if( post.magnitude > 0.01f )
+				this.transform.position = this.focusObj.transform.position + post;
+		}
+		
+		this.oldPos = mousePos;
+		
+		return;
+	}
+	
+	void cameraTranslate( Vector3 vec ) {
+		Transform focusTrans = this.focusObj.transform;
+		Transform trans = this.transform;
 
-        if (Mathf.Abs(rotation - 0.0f) < 0.00001f)
-        {
-            rotation = Input.GetAxis("Horizontal") * rotationSpeed;
-        }
-//#endif
+		focusTrans.Translate( ( trans.right * vec.x ) + ( trans.up * vec.y ) );
+		
+		return;
+	}
+	
+	public void cameraRotate( Vector3 eulerAngle ) {
+		Vector3 focusPos = this.focusObj.transform.position;
+		Transform trans = this.transform;
+		
+		Vector3 preUpV, preAngle, prePos;
+		preUpV = trans.up;
+		preAngle = trans.localEulerAngles;
+		prePos = trans.position;
+		
+		trans.RotateAround( focusPos, Vector3.up, eulerAngle.y );
 
-        // model sınırları dısına gitmesin diye kamera konumu kontrolu yapılacak!!!!!!!!!
+		trans.RotateAround( focusPos, trans.right, -eulerAngle.x );
+		
+		trans.LookAt( focusPos );
+		
+		Vector3 up = trans.up;
+		if( Vector3.Angle( preUpV, up ) > 90.0f ) {
+			trans.localEulerAngles = preAngle;
+			trans.position = prePos;
+		}
+		
+		return;
 
+		//Turn left and right with A and D key
+		if(Input.GetKey(KeyCode.D))
+		{
+			transform.Translate(new Vector3(speed * Time.deltaTime,0,0));
+		}
+		if(Input.GetKey(KeyCode.A))
+		{
+			transform.Translate(new Vector3(-speed * Time.deltaTime,0,0));
+		}
 
-        translation *= Time.deltaTime;
-        rotation *= Time.deltaTime;
+	}
+	void OnGUI()
+	{
+		int W = Screen.width,
+		H = Screen.height;
+		int w = W / 15,
+		h = H / 15;
+		int marginX = w / 2,
+		marginY = h;
+		int x, y;
+		
+		
+		GUIStyle style = new GUIStyle("button");
+		style.fontSize = 30;
+		
+		x = h + (w - h) / 2 + w + marginX;
+		y = H - (2 * h + w + h);
+		if (GUI.RepeatButton(new Rect(x, y, w, h), "Up", style))
+			flyFactor = 1;
+		x = h + (w - h) / 2 + w + marginX;
+		y = H - w;
+		if (GUI.RepeatButton(new Rect(x, y, w, h), "Down", style))
+			flyFactor = -1;
 
-        float translationY = flyFactor * speed * Time.deltaTime;
-        transform.Translate(0, translationY, translation);
-        flyFactor = 0;
-        transform.Rotate(0, rotation, 0);
-/*
-        if (Input.GetKey(KeyCode.LeftShift))
-            flyFactor = 1;
-        if (Input.GetKey(KeyCode.LeftControl))
-            flyFactor = -1;
+		if (Input.GetKey(KeyCode.LeftShift))
+			flyFactor = 1;
+		if (Input.GetKey(KeyCode.LeftControl))
+			flyFactor = -1;
 
-        float translation = Input.GetAxis("Vertical") * speed;
-        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
-
-        translation *= Time.deltaTime; // Zamanla ilerlemesi için
-        rotation *= Time.deltaTime;
-
-        float translationY = flyFactor * speed * Time.deltaTime;
-        transform.Translate(0, translationY, translation);
-        flyFactor = 0;
-
-        transform.Rotate(0, rotation, 0);
-*/
-    }
-
-
-
-    void OnGUI()
-    {
-        int W = Screen.width,
-            H = Screen.height;
-        int w = W / 15,
-            h = H / 15;
-        int marginX = w / 2,
-            marginY = h;
-        int x, y;
-
-
-        GUIStyle style = new GUIStyle("button");
-        style.fontSize = 30;
-
-        x = h + (w - h) / 2 + w + marginX;
-        y = H - (2 * h + w + h);
-        if (GUI.RepeatButton(new Rect(x, y, w, h), "Up", style))
-            flyFactor = 1;
-        x = h + (w - h) / 2 + w + marginX;
-        y = H - w;
-        if (GUI.RepeatButton(new Rect(x, y, w, h), "Down", style))
-            flyFactor = -1;
-
-#if ISTABLET
-
-        if (Input.GetKey(KeyCode.LeftShift))
-            flyFactor = 1;
-        if (Input.GetKey(KeyCode.LeftControl))
-            flyFactor = -1;
-#endif
-
-        // Left Button
-        x = W - (3 * w + marginX);
-        y = H - (3 * h);
-        if (GUI.RepeatButton(new Rect(x, y, w, h), "<", style))
-            horizontal = -1;
-
-        //Right Button
-        x = W - (w + marginX);
-        y = H - (3 * h);
-        if (GUI.RepeatButton(new Rect(x, y, w, h), ">", style))
-            horizontal = 1;
-
-        //Forward Button
-        x = W - (h + (w - h) / 2 + w + marginX);
-        y = H - (2 * h + w + h);
-
-        GUIUtility.RotateAroundPivot(-90, new Vector2(x + (h / 2), y + (w / 2)));
-        if (GUI.RepeatButton(new Rect(x, y, h, w), ">", style))
-            vertical = 1;
-
-        GUIUtility.RotateAroundPivot(90, new Vector2(x + (h / 2), y + (w / 2)));
-
-        //Backward Button
-        x = W - (h + (w - h) / 2 + w + marginX);
-        y = H - (w);
-        GUIUtility.RotateAroundPivot(-90, new Vector2(x + (h / 2), y + (w / 2)));
-        if (GUI.RepeatButton(new Rect(x, y, h, w), "<", style))
-            vertical = -1;
-
-       // Debug.Log("V = " + vertical + " H = " + horizontal);
-    }
+		#if ISTABLET
+		
+		if (Input.GetKey(KeyCode.LeftShift))
+			flyFactor = 1;
+		if (Input.GetKey(KeyCode.LeftControl))
+			flyFactor = -1;
+		#endif
+		
+		// Left Button
+		x = W - (3 * w + marginX);
+		y = H - (3 * h);
+		if (GUI.RepeatButton(new Rect(x, y, w, h), "<", style))
+			horizontal = -1;
+		
+		//Right Button
+		x = W - (w + marginX);
+		y = H - (3 * h);
+		if (GUI.RepeatButton(new Rect(x, y, w, h), ">", style))
+			horizontal = 1;
+		
+		//Forward Button
+		x = W - (h + (w - h) / 2 + w + marginX);
+		y = H - (2 * h + w + h);
+		
+		GUIUtility.RotateAroundPivot(-90, new Vector2(x + (h / 2), y + (w / 2)));
+		if (GUI.RepeatButton(new Rect(x, y, h, w), ">", style))
+			vertical = 1;
+		
+		GUIUtility.RotateAroundPivot(90, new Vector2(x + (h / 2), y + (w / 2)));
+		
+		//Backward Button
+		x = W - (h + (w - h) / 2 + w + marginX);
+		y = H - (w);
+		GUIUtility.RotateAroundPivot(-90, new Vector2(x + (h / 2), y + (w / 2)));
+		if (GUI.RepeatButton(new Rect(x, y, h, w), "<", style))
+			vertical = -1;
+		
+		// Debug.Log("V = " + vertical + " H = " + horizontal);
+	}
 }
--- MoveCameraForGearVR.cs
+++ MoveCameraForGearVR.cs
@@ -1,148 +1,91 @@
-﻿using UnityEngine;
+using UnityEngine;
 using VRStandardAssets.Utils;
-using UnityEngine.UI;
-
-/// <summary>
-/// Attach this script to an Empty with two children: "MainCamera prefab from VRSampleScenes/Prefabs/Utils" and also an Empty you should call "Body".
-/// This script is using the VR Input script from the VR Samples Unity Asset Store (Just drag the MainCamera onto the VR Input property on this script in the Inspector.
-/// 
-/// Makes your VrFps-player move in the direction/antidirection (??? what :-)) of your gaze by swiping left or right on your Gear VR's touchpad. (Still waiting for my controller from Ebay :-()
-/// Stops by swiping down or double click.  Quits the Game by pressing Back button on Gear VR.  
-/// 
-/// Written by harald.heide.gundersen@hotmail.com on 12/13/2015.
-/// Unity Verion 5.3.0f4 personal 
-/// Using VR Samples free from Unity Technologies
-/// Tested on a Samsung Galaxy S6 in GearVR S321 and Consumer GearVR S322.
-/// </summary>
 
 [RequireComponent(typeof (Rigidbody))]
-public class MoveCameraForGearVR : MonoBehaviour
+public class OVR_RB_Player : MonoBehaviour
 {
-	[SerializeField]
-	private float m_speed = 0.8f;
-	[SerializeField]
-	private VRInput m_VRInput;
+    [SerializeField]
+    private float m_speed = 0.8f;
+    [SerializeField]
+    private VRInput m_VRInput;
 
-	private Rigidbody m_Rigidbody;
-	private Transform head;
-	private Transform body;
+    private Rigidbody m_Rigidbody;
+    private Transform head;
+    private Transform body;
 
-	private int m_isWalking = 0;
-	private Vector3 m_direction;
-	public Image panel;
-	//public GameObject panelMap;
+    private int m_isWalking = 0;
+    private Vector3 m_direction;
 
-	[SerializeField] private VRInteractiveItem[] m_InteractiveItemBlock;
-	public GameObject panel1;
-	public GameObject panel2;
-	public GameObject panel3;
-	public GameObject panel4;
+    void Awake()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+        head = Camera.main.transform;
+        body = transform.Find("Body");
+    }
+	
+    private void OnEnable()
+    {
+        m_VRInput.OnSwipe += HandleSwipe;
+        m_VRInput.OnDoubleClick += HandleDoubleClick;
+        m_VRInput.OnCancel += HandleCancel;
+    }
 
-	GameObject obj1;
-	GameObject obj2;
-	GameObject obj3;
+    private void OnDisable()
+    {
+        m_VRInput.OnSwipe -= HandleSwipe;
+        m_VRInput.OnDoubleClick -= HandleDoubleClick;
+        m_VRInput.OnCancel -= HandleCancel;
+    }
 
-	// Use this for initialization
-	void Start () {
-		GameObject panel = GameObject.Find("Panel");
-	}
-		
-	void Awake()
-	{
-		m_Rigidbody = GetComponent<Rigidbody>();
-		head = Camera.main.transform;
-		body = transform.Find("Body");
-	}
-	private void OnEnable()
-	{
-		m_VRInput.OnSwipe += HandleSwipe;
-		//m_VRInput.OnDown += HandleDown;
-		//m_VRInput.OnUp += HandleUp;
-		//m_VRInput.OnClick += HandleClick;
-		m_VRInput.OnDoubleClick += HandleDoubleClick;
-		m_VRInput.OnCancel += HandleCancel;
-	}
+    private void HandleSwipe(VRInput.SwipeDirection swipeDirection)
+    {
+        switch (swipeDirection)
+        {
+            case VRInput.SwipeDirection.NONE:
+                break;
+            case VRInput.SwipeDirection.UP:
+				transform.position = new Vector3(transform.position.x, 75, transform.position.z);
+                break;
+            case VRInput.SwipeDirection.DOWN:
+                transform.position = new Vector3(transform.position.x, 1, transform.position.z);
+                break;
+            case VRInput.SwipeDirection.LEFT:
+                m_isWalking += 2;
+                break;
+            case VRInput.SwipeDirection.RIGHT:
+                m_isWalking += -2;
+                break;
+        }
+    }
 
-	private void OnDisable()
-	{
-		m_VRInput.OnSwipe -= HandleSwipe;
-		//m_VRInput.OnDown -= HandleDown;
-		//m_VRInput.OnUp -= HandleUp;
-		//m_VRInput.OnClick -= HandleClick;
-		m_VRInput.OnDoubleClick -= HandleDoubleClick;
-		m_VRInput.OnCancel -= HandleCancel;
-	}
+    private void HandleDown()
+    {
+    }
+    private void HandleUp()
+    {
+    }
+    private void HandleClick() // Quits the Game by pressing Back button on Gear VR
+    {
+    }
 
-	private void HandleSwipe(VRInput.SwipeDirection swipeDirection)
-	{
-		switch (swipeDirection)
-		{
-		case VRInput.SwipeDirection.NONE:
-			break;
-		case VRInput.SwipeDirection.UP:
-			transform.position = new Vector3(transform.position.x, 75, transform.position.z);
-			break;
-		case VRInput.SwipeDirection.DOWN:
-			transform.position = new Vector3(transform.position.x, 1, transform.position.z);
-			break;
-		case VRInput.SwipeDirection.LEFT:
-			m_isWalking += 1;
-			break;
-		case VRInput.SwipeDirection.RIGHT:
-			m_isWalking += -1;
-			break;
-		}
-	}
+    private void HandleDoubleClick() // Stops by swiping down or double click.
+    {
+        m_isWalking = 0;
+    }
 
-	private void HandleDown()
-	{
-	}
-	private void HandleUp()
-	{
-		
-	}
-	private void HandleClick()
-	{
-		
-	}
+    private void HandleCancel()
+    {
+        Application.Quit();
+    }
 
-	private void HandleDoubleClick()
-	{
-		m_isWalking = 0;
-		panel.gameObject.SetActive (false);
-		//panelMap.SetActive (false);
-		m_InteractiveItemBlock[0].gameObject.SetActive (false);
-		m_InteractiveItemBlock[1].gameObject.SetActive (false);
-		m_InteractiveItemBlock[2].gameObject.SetActive (false);
-		m_InteractiveItemBlock[3].gameObject.SetActive (false);
-		//m_InteractiveItemBlock[4].gameObject.SetActive (false);
-		//m_InteractiveItemBlock[5].gameObject.SetActive (false);
-		//m_InteractiveItemBlock[5].gameObject.SetActive (true);
-
-		panel1.SetActive (false);
-		panel2.SetActive (false);
-		panel3.SetActive (false);
-		panel4.SetActive (false);
-	}
-
-	private void HandleCancel()
-	{
-		Application.Quit();
-
-	}
-
-	public void FixedUpdate()
-	{
-		if (m_isWalking != 0)
-		{
-			m_direction = head.forward;
-			m_direction.y = 0f;
-			m_Rigidbody.transform.Translate(m_direction * m_speed * m_isWalking * Time.fixedDeltaTime);
-			body.transform.rotation = Quaternion.Euler(new Vector3(0.0f, head.eulerAngles.y, 0.0f));
-		}
-	}
-
-	public void setm_isWalking() {
-		m_isWalking = 0;
-	}
+    public void FixedUpdate()
+    {
+        if (m_isWalking != 0)
+        {
+            m_direction = head.forward;
+            m_direction.y = 0f;
+            m_Rigidbody.transform.Translate(m_direction * m_speed * m_isWalking * Time.fixedDeltaTime);
+            body.transform.rotation = Quaternion.Euler(new Vector3(0.0f, head.eulerAngles.y, 0.0f));
+        }
+    }
 }
